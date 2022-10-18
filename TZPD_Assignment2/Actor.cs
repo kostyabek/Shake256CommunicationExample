399b2d4f7d1ccf9be286353b39642ac1106de14f
kostyabek <kostyabek@gmail.com>
2022-10-18 19:03:57 +0300

    Add project files.

---
+++ TZPD_Assignment2/Actor.cs
@@ -0,0 +1,98 @@
+﻿using Core.Security.Cryptography;
+using Org.BouncyCastle.Asn1.Nist;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Signers;
+using Org.BouncyCastle.Security;
+
+namespace TZPD_Assignment2;
+
+public class Actor
+{
+    public AsymmetricKeyParameter PublicKey { get; }
+    private AsymmetricKeyParameter PrivateKey { get; }
+
+    private const string EncryptionAlgorithm = "RSA/ECB/OAEPWithSHA256AndMGF1Padding";
+    private const int DefaultRsaBlockSize = 256;
+
+    public Actor()
+    {
+        var keyPair = GenerateKeys();
+        PublicKey = keyPair.Public;
+        PrivateKey = keyPair.Private;
+    }
+
+    public IEnumerable<byte> Encrypt(AsymmetricKeyParameter publicKey, byte[] dataToEncrypt)
+    {
+        var cipher = CipherUtilities.GetCipher(EncryptionAlgorithm);
+        cipher.Init(true, publicKey);
+        return ApplyCipher(dataToEncrypt, cipher, DefaultRsaBlockSize);
+    }
+
+    public byte[] Hash(byte[] dataToHash, int hashLength)
+    {
+        using var hashAlgorithm = new Shake256Managed(hashLength);
+        var hashedData = hashAlgorithm.ComputeHash(dataToHash);
+
+        return hashedData;
+    }
+
+    public byte[] Sign(byte[] dataToSign)
+    {
+        var digest = new ShakeDigest(DefaultRsaBlockSize);
+        digest.BlockUpdate(dataToSign, 0, dataToSign.Length);
+
+        var signer = new RsaDigestSigner(digest, NistObjectIdentifiers.IdShake256);
+        signer.Init(true, PrivateKey);
+        signer.BlockUpdate(dataToSign, 0, dataToSign.Length);
+
+        return signer.GenerateSignature();
+    }
+
+    public bool VerifySignature(AsymmetricKeyParameter publicKey, byte[] signedData, byte[] signature)
+    {
+        var digest = new ShakeDigest(DefaultRsaBlockSize);
+        digest.BlockUpdate(signedData, 0, signedData.Length);
+
+        var signer = new RsaDigestSigner(digest, NistObjectIdentifiers.IdShake256);
+        signer.Init(false, publicKey);
+        signer.BlockUpdate(signedData, 0, signedData.Length);
+
+        return signer.VerifySignature(signature);
+    }
+
+    public IEnumerable<byte> DecryptData(byte[] encryptedData)
+    {
+        var cipher = CipherUtilities.GetCipher(EncryptionAlgorithm);
+        cipher.Init(false, PrivateKey);
+
+        return ApplyCipher(encryptedData, cipher, DefaultRsaBlockSize);
+    }
+
+    private AsymmetricCipherKeyPair GenerateKeys()
+    {
+        var random = new SecureRandom();
+        var keyGenerationParameters = new KeyGenerationParameters(random, 2048);
+        var generator = new RsaKeyPairGenerator();
+        generator.Init(keyGenerationParameters);
+
+        return generator.GenerateKeyPair();
+    }
+
+    private IEnumerable<byte> ApplyCipher(byte[] data, IBufferedCipher cipher, int blockSize)
+    {
+        var inputStream = new MemoryStream(data);
+        var outputBytes = new List<byte>();
+
+        int index;
+        var buffer = new byte[blockSize];
+        while ((index = inputStream.Read(buffer, 0, blockSize)) > 0)
+        {
+            var cipherBlock = cipher.DoFinal(buffer, 0, index);
+            outputBytes.AddRange(cipherBlock);
+        }
+
+        return outputBytes.ToArray();
+    }
+}
---
+++ TZPD_Assignment2/Program.cs
@@ -0,0 +1,115 @@
+﻿using System.Text;
+
+namespace TZPD_Assignment2;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        const int hashLength = 256;
+        var initialInput = GenerateInputData('#');
+        var stringData = ConvertInputDataToString(initialInput);
+        var dataBytes = GetStringDataBytes(stringData);
+
+        var alice = new Actor();
+        var bob = new Actor();
+
+        var dataBytesChunks = dataBytes.Chunk(32);
+        var encryptedData = new byte[32768];
+
+        var index = 0;
+        foreach (var chunk in dataBytesChunks)
+        {
+            var encryptedChunk = alice.Encrypt(bob.PublicKey, chunk);
+            foreach (var b in encryptedChunk)
+            {
+                encryptedData[index] = b;
+                index++;
+            }
+        }
+
+        index = 0;
+
+        var hashedEncryptedData = alice.Hash(encryptedData, hashLength);
+        var signature = alice.Sign(hashedEncryptedData);
+        var decryptedData = new byte[encryptedData.Length];
+
+        var isValid = bob.VerifySignature(alice.PublicKey, hashedEncryptedData, signature);
+        if (isValid)
+        {
+            var encryptedDataChunks = encryptedData.Chunk(256);
+            foreach (var chunk in encryptedDataChunks)
+            {
+                var decryptedChunk = bob.DecryptData(chunk);
+                foreach (var b in decryptedChunk)
+                {
+                    decryptedData[index] = b;
+                    index++;
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Signature is not valid");
+        }
+
+        WriteLine("Input", stringData);
+        WriteLine("Encrypted input", BitConverter.ToString(encryptedData).Replace("-", string.Empty));
+        WriteLine("Hashed encrypted input", BitConverter.ToString(hashedEncryptedData).Replace("-", string.Empty));
+        WriteLine("Signature", BitConverter.ToString(signature).Replace("-", string.Empty));
+        WriteLine("Decrypted data", Encoding.UTF8.GetString(decryptedData));
+
+        Console.Read();
+    }
+
+    private static char[,] GenerateInputData(char delimiter)
+    {
+        var seed = (int)DateTime.UtcNow.Ticks;
+        var random = new Random(seed);
+        var data = new char[64, 64];
+        var allowedChars = new[] { '0', '1' };
+
+        for (var i = 0; i < data.GetLength(0); i++)
+        {
+            for (var j = 0; j < data.GetLength(1); j++)
+            {
+                if (i == data.GetLength(0) / 2 && j == data.GetLength(1) / 2)
+                {
+                    data[i, j] = delimiter;
+                    continue;
+                }
+
+                data[i, j] = allowedChars[random.Next(0, 2)];
+            }
+        }
+
+        return data;
+    }
+
+    private static string ConvertInputDataToString(char[,] data)
+    {
+        var stringBuilder = new StringBuilder();
+        foreach (var c in data)
+        {
+            stringBuilder.Append(c);
+        }
+
+        return stringBuilder.ToString();
+    }
+
+    private static IEnumerable<byte> GetStringDataBytes(string data)
+        => Encoding.UTF8.GetBytes(data);
+
+    private static void WriteLine(string title, string text)
+    {
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine(title);
+
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine(text);
+
+        Console.WriteLine();
+    }
+}
